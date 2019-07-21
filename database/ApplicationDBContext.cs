b26dadb478a1a39f90fdeac94dc6ac4c2b18b0ca
luan <luan.desouzasantos@gmail.com>
2019-07-21 16:34:40 -0300

    commit versao totvs

---
+++ database/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using apiPDV.model;
+using Microsoft.EntityFrameworkCore;
+namespace apiPDV.database
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Change> Changes { get; set; }
+
+    }
+}
+
---
+++ model/Change.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiPDV.model
+{
+    public class Change
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public double value { get; set; }
+
+        public string Note { get; set; }
+    }
+}
